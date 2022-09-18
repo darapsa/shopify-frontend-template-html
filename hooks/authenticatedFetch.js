a7b4ce7e5a00927bf833efb84cca45e0bb94a7cc
ꦌꦫꦶꦏ꧀ꦦꦿꦧꦮꦑꦩꦭ꧀ <erik@darapsa.co.id>
2022-09-18 22:24:03 +0800

    It's not returning use

--- hooks/authenticatedFetch.js
+++ hooks/authenticatedFetch.js
@@ -1,25 +1,23 @@
-function authenticatedFetch(uri, options) {
+async function authenticatedFetch(uri, options) {
 	var AppBridge = window['app-bridge'];
 	var app = AppBridge.createApp({
 		apiKey: '',
 		host: ''
 	});
 	var fetchFunction = window['app-bridge-utils'].authenticatedFetch(app);
-	return async (uri, options) => {
-		var response = await fetchFunction(uri, options);
-		var headers = response.headers;
-		if (headers.get("X-Shopify-API-Request-Failure-Reauthorize")
-			=== "1") {
-			var authUrlHeader = headers.get(
-				"X-Shopify-API-Request-Failure-Reauthorize-Url")
-				|| `/api/auth`;
-			var Redirect = AppBridge.actions.Redirect;
-			Redirect.create(app).dispatch(Redirect.Action.REMOTE,
-				authUrlHeader.startsWith("/")
-				? `https://${window.location.host}${authUrlHeader}`
-				: authUrlHeader
-			);
-		}
-		return response;
-	};
+	var response = await fetchFunction(uri, options);
+	var headers = response.headers;
+	if (headers.get("X-Shopify-API-Request-Failure-Reauthorize")
+		=== "1") {
+		var authUrlHeader = headers.get(
+			"X-Shopify-API-Request-Failure-Reauthorize-Url")
+			|| `/api/auth`;
+		var Redirect = AppBridge.actions.Redirect;
+		Redirect.create(app).dispatch(Redirect.Action.REMOTE,
+			authUrlHeader.startsWith("/")
+			? `https://${window.location.host}${authUrlHeader}`
+			: authUrlHeader
+		);
+	}
+	return response;
 }
